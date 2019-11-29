de23ac9a98a71911e06502af65a310f42510b88b
romanb123 <romanb123@users.noreply.github.com>
2019-11-29 17:10:52 +0200

    fghfghfgh

--- frontend/my-app/src/app/auth/login/login.component.ts
+++ frontend/my-app/src/app/auth/login/login.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { AuthService } from "../auth.servise";
+import { Productservice } from "../../products/product-service";
+import { Subscription } from 'rxjs';
+
 
 @Component({
-templateUrl:'./login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class Logincomponent{
+export class Logincomponent implements OnInit {
+  private productsub: Subscription;
+  productslenth:any;
 
-   loading = false;
-   constructor(public authService: AuthService) {}
+  loading = false;
+  constructor(public authService: AuthService, public productservice: Productservice) { }
 
 
-    onLogin(form: NgForm) {
-        if (form.invalid) {
-          return;
-        }
-        this.loading = true;
-        this.authService.login(form.value.email, form.value.password);
-      }
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.loading = true;
+    this.authService.login(form.value.email, form.value.password);
+  }
+
+  ngOnInit() {
+    this.productservice.getproduct();
+    this.productsub = this.productservice.productUpdatelistener()
+      .subscribe((Product) => {
+        this.loading = false;
+        this.productslenth = Product.length;
+        console.log(this.productslenth);
+      });
+  }
 }
